f557ee38f631af179eb30d1fdf634e9946b86852
Silas Barta <sbarta@gmail.com>
2017-10-24 22:38:55 -0700

    App correctly responds to clicking on the name.

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const list = ['a', 'b', 'c'];
-}, false);
-
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -12,17 +8,38 @@ function correct() {
   alert("Correct!");
 }
 
-$(document).ready( function() {
-  // names from names.js
+function wrong() {
+  alert("Wrong!");
+}
+
+function initializeChallenge() {
+  // Clear contents of #guesslist
+  $('#guesslist').html("");
   const correct_index = getRandomInt(0, names.length);
   const name = names[correct_index];
   const filepath = "images/B_" + name + ".JPG";
-  const image_elem = '<img width="200px" id="pic" src="' + filepath + '">';
-  console.log(image_elem);
+  const image_elem = '<img id="pic" src="' + filepath + '">';
   $('#pic').html(image_elem);
   //var guess_values = [];
   //$().append('<input type="radio" name="nameguess"> Name1<br>');
-  correct_line = '<input type="radio" name="nameguess" onClick="correct()">' +  name + '<br>'
-      //$('#guesslist').append(guess_line)
-  $('#guesslist').append(correct_line)
+  const correct_line = '<input type="radio" name="nameguess" onClick="correct()">' +  name + '<br>';
+  correct_name_position = getRandomInt(0, 4);
+  // Append four radio-button choices to the 
+  for (var i=0; i < 4; i++) {
+    var wrong_index = getRandomInt(0, names.length);
+    // move it one if it got the correct index, modulo the name length
+    if (i === correct_name_position) {
+      $('#guesslist').append(correct_line);
+    } else {
+      if (wrong_index === correct_index) {
+        wrong_index = (wrong_index + 1) % names.length;
+      }
+      var wrong_line = '<input type="radio" name="nameguess" onClick="wrong()">' + names[wrong_index] + '<br>';
+      $('#guesslist').append(wrong_line)
+    }
+  }
+}
+
+$(document).ready( function() {
+  initializeChallenge();
 })
